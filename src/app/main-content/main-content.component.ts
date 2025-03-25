784bf3f165adbdf5708477a8991b868b49f987aa
Maximilian Wagener <159955254+DeVAiRMaX@users.noreply.github.com>
2025-03-25 11:35:02 +0100

    #4

--- src/app/main-content/main-content.component.ts
+++ src/app/main-content/main-content.component.ts
@@ -4,11 +4,19 @@ import { SlideshowComponent } from './slideshow/slideshow.component';
 import { ClientsComponent } from './clients/clients.component';
 import { DescriptionComponent } from './description/description.component';
 import { InfoComponent } from './info/info.component';
+import { HelpingComponent } from './helping/helping.component';
 
 @Component({
   selector: 'app-main-content',
   standalone: true,
-  imports: [HeaderComponent, SlideshowComponent, ClientsComponent, DescriptionComponent, InfoComponent],
+  imports: [
+    HeaderComponent,
+    SlideshowComponent,
+    ClientsComponent,
+    DescriptionComponent,
+    InfoComponent,
+    HelpingComponent,
+  ],
   templateUrl: './main-content.component.html',
   styleUrl: './main-content.component.scss'
 })
